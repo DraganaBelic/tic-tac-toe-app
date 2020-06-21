030640f9500dc93500ea74964ba08a96374f73ec
unknown <draganabelic.mz@gmail.com>
2020-06-21 11:17:03 +0200

    Updates

---
+++ precache-manifest.9e715ac5aaa0f37b73386b64e476af99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "68bf491f9c9bfea1ab31",
+    "url": "/static/css/main.6eead802.chunk.css"
+  },
+  {
+    "revision": "68bf491f9c9bfea1ab31",
+    "url": "/static/js/main.95d821c5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d906d70496ed9550ecd",
+    "url": "/static/css/2.b2498f8a.chunk.css"
+  },
+  {
+    "revision": "5d906d70496ed9550ecd",
+    "url": "/static/js/2.41b65196.chunk.js"
+  },
+  {
+    "revision": "ec030adf4cd57ff43a5ade3972775472",
+    "url": "/index.html"
+  }
+];
